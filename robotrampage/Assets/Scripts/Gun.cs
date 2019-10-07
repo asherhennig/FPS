4f59adc0b919fab14eac7f4132dc9e03ded26f70
Asher Hennig [Student] <asher.hennig@student.sl.on.ca>
2019-10-07 15:13:11 -0400

    added gun rate of fire

--- robotrampage/Assets/Scripts/Gun.cs
+++ robotrampage/Assets/Scripts/Gun.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Gun : MonoBehaviour
 {
+    public float fireRate;
+    protected float lastFireTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastFireTime = Time.time - 10;
     }
 
     // Update is called once per frame
-    void Update()
+    protected virtual void Update()
     {
-        
+
+    }
+
+    protected void Fire()
+    {
+
     }
 }
