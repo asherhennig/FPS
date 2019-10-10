91f4f4aaa91335229ef788dd0b79f2be1b4dd393
asher hennig <asherhennig@gmail.com>
2019-10-09 21:09:43 -0400

    added pick ups

--- robotrampage/Assets/Scripts/Player.cs
+++ robotrampage/Assets/Scripts/Player.cs
@@ -47,6 +47,61 @@ public class Player : MonoBehaviour
         }
     }
 
+    //1 Adds health and armour
+    private void pickupHealth()
+    {
+        health += 50;
+        if (health > 200)
+        {
+            health = 200;
+        }
+    }
+
+    private void pickupArmour()
+    {
+        armor += 15;
+    }
+
+    //2 Adds ammo
+    private void pickupAssaultRifleAmmo()
+    {
+        ammo.AddAmmo(Constants.AssaultRifle, 50);
+    }
+
+    private void pickupPistolAmmo()
+    {
+        ammo.AddAmmo(Constants.Pistol, 20);
+    }
+
+    private void pickupShotgunAmmo()
+    {
+        ammo.AddAmmo(Constants.Shotgun, 10);
+    }
+
+    public void PickUpItem(int pickupType)
+    {
+        switch (pickupType)
+        {
+            case Constants.PickUpArmor:
+                pickupArmour();
+                break;
+            case Constants.PickUpHealth:
+                pickupHealth();
+                break;
+            case Constants.PickUpAssaultRifleAmmo:
+                pickupAssaultRifleAmmo();
+                break;
+            case Constants.PickUpPistolAmmo:
+                pickupPistolAmmo();
+                break;
+            case Constants.PickUpShotgunAmmo:
+                pickupShotgunAmmo();
+                break;
+            default:
+                Debug.LogError("Bad pick type passed" + pickupType);
+                break;
+        }
+    }
 
     // Update is called once per frame
     void Update()
