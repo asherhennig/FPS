9e893feb976be07d5eb72edd2fc708d8e3e01a26
asher hennig <asherhennig@gmail.com>
2019-10-09 22:43:50 -0400

    added robot spawning

---
+++ robotrampage/Assets/Scripts/Game.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    private static Game singleton;
+
+    [SerializeField]
+    RobotSpawn[] spawns;
+
+    public int enemiesLeft;
+    //1 Start is called before the first frame update
+    void Start()
+    {
+        singleton = this;
+        SpawnRobots();
+    }
+
+    private void SpawnRobots()
+    {
+        foreach (RobotSpawn spawn in spawns)
+        {
+            spawn.SpawnRobot();
+            enemiesLeft++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- robotrampage/Assets/Scripts/Robot.cs
+++ robotrampage/Assets/Scripts/Robot.cs
@@ -10,6 +10,13 @@ public class Robot : MonoBehaviour
     [SerializeField]
     GameObject missileprefab;
 
+    [SerializeField]
+    private AudioClip deathSound;
+    [SerializeField]
+    private AudioClip fireSound;
+    [SerializeField]
+    private AudioClip weakHitSound;
+
     public int health;
     public int range;
     public float fireRate;
@@ -64,6 +71,8 @@ public class Robot : MonoBehaviour
         missile.transform.position = missileFireSpot.transform.position;
         missile.transform.rotation = missileFireSpot.transform.rotation;
         robot.Play("Fire");
+
+        GetComponent<AudioSource>().PlayOneShot(fireSound);
     }
 
     public void TakeDamage(int amount)
@@ -80,6 +89,12 @@ public class Robot : MonoBehaviour
             isDead = true;
             robot.Play("Die");
             StartCoroutine("DestroyRobot");
+
+            GetComponent<AudioSource>().PlayOneShot(deathSound);
+        }
+        else
+        {
+            GetComponent<AudioSource>().PlayOneShot(weakHitSound);
         }
     }
 
